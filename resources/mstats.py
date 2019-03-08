47c7b02d654ba8562f8bbaf07066af31a6dc0d85
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2019-03-08 05:52:19 -0500

    anova

--- resources/mstats.py
+++ resources/mstats.py
@@ -844,7 +844,14 @@ class hypothesis:
         
         print("You must input a valid tail ('two', 'left' or 'right')")
         
-        
+    
+    def crit_val_f(df1, df2, sign):
+        """
+        """
+        alpha = 1 - sign
+        crit_val = ss.f.ppf(alpha, df1, df2)
+        return crit_val
+                
         
     def reject_h0(crit_val, value, tail):
         """
@@ -910,6 +917,174 @@ class hypothesis:
 
             
     
+#------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------#
+            
+#------------------------------------------------------------------------------------------------#
+#------------------------------------------- ANOVA ----------------------------------------------#
+#------------------------------------------------------------------------------------------------#        
+class anova:
+    
+    
+    def get_sct(*args):
+        """
+        """
+        arr_tot = np.concatenate(args)
+        big_mean = np.mean(arr_tot)
+        sct = sum([(x - big_mean)**2 for x in arr_tot])
+        return sct
+        
+    
+        
+        #d1 = [cond1[x] - cond2[x] for x in range(n)]
+        #d21 = [x**2 for x in d1]
+        
+    def get_sctr(*args):
+        """
+        """
+        arr_tot = np.concatenate(args)
+        big_mean = np.mean(arr_tot)
+        sctr = sum([(len(x) * ((np.mean(x) - big_mean)**2)) for x in args])
+        return sctr
+       
+        
+    def get_sce(*args):
+        """
+        """        
+        sce = 0
+        
+        for x in args:
+            for xj in x:
+                sce += (xj - np.mean(x))**2
+        return sce
+    
+    def get_sce2(*args):
+        """
+        """
+        sct = anova.get_sct(*args)
+        sctr = anova.get_sctr(*args)
+        scbl = anova.get_scbl(*args)
+        
+        sce = sct - sctr - scbl
+        return sce
+    
+    def get_scbl(*args):
+        """
+        """
+        lists = args
+        blocks = list(map(list, zip(*lists)))
+        arr_tot = np.concatenate(blocks)
+        big_mean = np.mean(arr_tot)
+        scbl = sum([(len(x) * ((np.mean(x) - big_mean)**2)) for x in blocks])
+        
+        return scbl
+        
+    
+    def get_cmt(*args):
+        """
+        """
+        arr_tot = np.concatenate(args)
+        df = len(arr_tot) - 1
+        sct = anova.get_sct(*args)
+        cmt = sct / df
+        return cmt
+    
+    def get_cmtr(*args):
+        """
+        """
+        sctr = anova.get_sctr(*args)
+        df = len(args) - 1
+        cmtr = sctr / df
+        return cmtr
+    
+    def get_cme(*args):
+        """
+        """
+        arr_tot = np.concatenate(args)
+        sce = anova.get_sce(*args)
+        df = len(arr_tot) - len(args)
+        cme = sce / df
+        return cme
+    
+    def get_cme2(*args):
+        """
+        """
+        sce = anova.get_sce2(*args)
+        df = (len(args) - 1) * (len(args[0]) - 1)
+        cme = sce / df
+        return cme
+    
+    def get_cmbl(*args):
+        """
+        """
+        scbl = anova.get_scbl(*args)
+        df = len(args[0]) - 1
+        cmbl = scbl / df
+        return cmbl
+        
+    def get_fratio(*args):
+        """
+        """
+        cmtr = anova.get_cmtr(*args)
+        cme = anova.get_cme(*args)
+        fratio = cmtr / cme
+        return fratio
+    
+    def get_fratio2(*args):
+        """
+        """
+        cmtr = anova.get_cmtr(*args)
+        cme = anova.get_cme2(*args)
+        fratio = cmtr / cme
+        return fratio
+    
+    def get_fratio2_cmbl(*args):
+        """
+        """
+        cmbl = anova.get_cmbl(*args)
+        cme = anova.get_cme2(*args)
+        fratio = cmbl / cme
+        return fratio
+    
+    def get_dms(*args, sign):
+        """
+        """
+        cme = anova.get_cme(*args)
+        
+        
+        for x in range(len(args)):
+            for xj in range(x+1, len(args)):
+                if(len(args[x]) == len(args[xj])):
+                    fratio = ss.f.ppf((1 - sign), 1, (len(np.concatenate(args)) - len(args)))
+                    dms = (((1 / len(args[x])) + (1 / len(args[xj]))) * cme * fratio)**0.5
+                    x_mean = np.mean(args[x])
+                    xj_mean = np.mean(args[xj])
+                    x_diff = abs(x_mean - xj_mean)
+                    if (x_diff > dms):
+                        print("|x_{} - x_{}| = |{} - {}| = {} > {}".format(x+1, xj+1,
+                              x_mean, xj_mean, x_diff, dms))
+                    else:
+                        print("|x_{} - x_{}| = |{} - {}| = {} <= {}".format(x+1, xj+1,
+                              x_mean, xj_mean, x_diff, dms))
+                else:
+                    fratio = ss.f.ppf((1 - sign), (len(args) - 1), (len(np.concatenate(args)) - len(args)))
+                    dms = (((1 / len(args[x])) + (1 / len(args[xj]))) * cme * fratio)**0.5
+                    x_mean = np.mean(args[x])
+                    xj_mean = np.mean(args[xj])
+                    x_diff = abs(x_mean - xj_mean)
+                    if (x_diff > dms):
+                        print("|x_{} - x_{}| = |{} - {}| = {} > {}".format(x+1, xj+1,
+                              x_mean, xj_mean, x_diff, dms))
+                    else:
+                        print("|x_{} - x_{}| = |{} - {}| = {} <= {}".format(x+1, xj+1,
+                              x_mean, xj_mean, x_diff, dms))
+        
+    
+        
+    
+            
+        
 #------------------------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------------------------#
